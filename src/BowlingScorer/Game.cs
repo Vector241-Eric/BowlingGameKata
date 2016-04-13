019f9e2ec9c6d7858b0e6f29a5b1469339d33a36
Eric Anderson <eric@vector241.com>
2016-04-13 08:58:51 -0500

    Spare test passes with ugly code

--- src/BowlingScorer/Game.cs
+++ src/BowlingScorer/Game.cs
@@ -3,10 +3,29 @@
     public class Game
     {
         private int _score;
+        private bool _isSecondRoll;
+        private int _firstRollInFrame;
+        private bool _hadSpare;
 
         public void AddRoll(int pins)
         {
+            if (_isSecondRoll)
+            {
+                if (pins + _firstRollInFrame == 10)
+                    _hadSpare = true;
+                _firstRollInFrame = 0;
+            }
+            else
+            {
+                if (_hadSpare)
+                {
+                    _score += pins;
+                    _hadSpare = false;
+                }
+                _firstRollInFrame = pins;
+            }
             _score += pins;
+            _isSecondRoll = !_isSecondRoll;
         }
 
         public int GetScore()
