826560f45335678503062e098d642cda5e2a8db4
Eric Anderson <eric@vector241.com>
2016-04-13 09:19:27 -0500

    Strike test passes

--- src/BowlingScorer/Game.cs
+++ src/BowlingScorer/Game.cs
@@ -14,22 +14,31 @@
         {
             var score = 0;
             var rollIndex = 0;
-            while (rollIndex <= 19)
+            for(int frame = 1; frame <= 10; frame++)
             {
-                //Spare
-                if (_rolls[rollIndex] + _rolls[rollIndex + 1] == 10)
+                //Stike
+                if (_rolls[rollIndex] == 10)
                 {
                     score += _rolls[rollIndex];
                     score += _rolls[rollIndex + 1];
                     score += _rolls[rollIndex + 2];
+                    rollIndex += 1;
+                }
+                //Spare
+                else if (_rolls[rollIndex] + _rolls[rollIndex + 1] == 10)
+                {
+                    score += _rolls[rollIndex];
+                    score += _rolls[rollIndex + 1];
+                    score += _rolls[rollIndex + 2];
+                    rollIndex += 2;
                 }
                 //Regular Case
                 else
                 {
                     score += _rolls[rollIndex];
                     score += _rolls[rollIndex + 1];
+                    rollIndex += 2;
                 }
-                rollIndex += 2;
             }
             return score;
         }
