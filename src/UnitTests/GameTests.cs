e833576ffafa7f7760a91010c37c83518f38be0a
Eric Anderson <eric@vector241.com>
2016-04-13 08:31:38 -0500

    First Test

--- src/UnitTests/GameTests.cs
+++ src/UnitTests/GameTests.cs
@@ -1,7 +1,23 @@
-﻿namespace UnitTests
+﻿using BowlingScorer;
+using NUnit.Framework;
+using Should;
+
+namespace UnitTests
 {
     public class GameTests
     {
-         
+        [TestFixture]
+        public class When_all_zeros_are_rolled
+        {
+            [Test]
+            public void Should_score_a_zero()
+            {
+                var game = new Game();
+                for (int roll = 0; roll < 20; roll++)
+                    game.AddRoll(0);
+
+                game.GetScore().ShouldEqual(0);
+            }
+        }
     }
 }
