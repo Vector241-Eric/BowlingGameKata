90177809f3d1e2b6100733e0fd46f19925d1597a
Eric Anderson <eric@vector241.com>
2016-04-13 08:32:01 -0500

    First test compiles

--- src/BowlingScorer/Game.cs
+++ src/BowlingScorer/Game.cs
@@ -2,6 +2,14 @@
 {
     public class Game
     {
-         
+        public void AddRoll(int pins)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetScore()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
