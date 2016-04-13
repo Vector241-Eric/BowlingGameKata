2f22899b2e152cc938991065b01d1c46ec112656
Eric Anderson <eric@vector241.com>
2016-04-13 08:34:31 -0500

    Second test

--- src/UnitTests/GameTests.cs
+++ src/UnitTests/GameTests.cs
@@ -19,5 +19,19 @@ namespace UnitTests
                 game.GetScore().ShouldEqual(0);
             }
         }
+
+        [TestFixture]
+        public class When_all_ones_are_rolled
+        {
+            [Test]
+            public void Should_score_a_20()
+            {
+                var game = new Game();
+                for (int roll = 0; roll < 20; roll++)
+                    game.AddRoll(1);
+
+                game.GetScore().ShouldEqual(20);
+            }
+        }
     }
 }
